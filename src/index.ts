93634bc8993cca50fad42d766729918423a159ac
escapeadmire <147107022+escapeadmire@users.noreply.github.com>
2025-03-04 16:39:05 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -63,9 +63,9 @@ export default {
         primary: "#0051c3",
         favicon: "https://workers.cloudflare.com//favicon.ico",
         logo: {
-          dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
+          dark: "https://solecheck.info/media/images/logo.png",
           light:
-            "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
+            "https://solecheck.info/media/images/logo-black.png",
         },
       },
       success: async (ctx, value) => {
